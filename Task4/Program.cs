1c75f0580fd66219d14f9123e9f38fec4c24ada4
adrenaline <adrenaline9093@gmail.com>
2024-02-13 21:54:05 +0300

    add less1;Task4 цыклы больше меньше

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 5;
+int b = 20;
+int c = 13;
+int d = 30;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+max = b;
+}
+if (c > max)
+{
+max = c;
+}if (d > max)
+{
+max = d;
+}if (e > max)
+{
+max = e;
+}
+Console.WriteLine(max);
